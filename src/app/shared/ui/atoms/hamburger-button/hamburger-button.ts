442d748f2c4c153e5f7e26a57b2cae5920fd0c61
Daniella <2610daniella@gmail.com>
2025-07-07 15:52:25 +0200

    Added UI atoms

---
+++ src/app/shared/ui/atoms/hamburger-button/hamburger-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger-button',
+  imports: [],
+  templateUrl: './hamburger-button.html',
+  styles: ``
+})
+export class HamburgerButton {
+
+}
